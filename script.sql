035af40d31481702b3eb42a6e9103bab0f0e9669
Leandro Liduvino <leandro.augusto460@gmail.com>
2024-04-06 12:43:40 -0300

    chore: update column names

--- script.sql
+++ script.sql
@@ -1,19 +1,19 @@
-SET timezone = 'America/Sao_Paulo';
-
+DROP TABLE IF EXISTS users;
 CREATE TABLE users
 (
     id              SERIAL PRIMARY KEY,
-    limit_in_cents  INTEGER NOT NULL,
-    initial_balance INTEGER NOT NULL DEFAULT 0
+    user_limit      INTEGER NOT NULL,
+    user_balance    INTEGER NOT NULL DEFAULT 0
 );
 
-INSERT INTO users (id, limit_in_cents, initial_balance)
+INSERT INTO users (id, user_limit, user_balance)
 VALUES (DEFAULT, 1000 * 100, 0),
        (DEFAULT, 800 * 100, 0),
        (DEFAULT, 10000 * 100, 0),
        (DEFAULT, 100000 * 100, 0),
        (DEFAULT, 5000 * 100, 0);
 
+DROP TABLE IF EXISTS transactions;
 CREATE UNLOGGED TABLE transactions
 (
     id          SERIAL PRIMARY KEY,
@@ -21,7 +21,7 @@ CREATE UNLOGGED TABLE transactions
     value       INTEGER     NOT NULL,
     type        CHAR(1)     NOT NULL,
     description VARCHAR(10) NOT NULL,
-    do_at       TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at       TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 ALTER TABLE
