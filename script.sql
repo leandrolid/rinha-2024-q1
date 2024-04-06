1c860a5deee0412a2105b070073a5180b6a40e91
Leandro Liduvino <leandro.augusto460@gmail.com>
2024-04-05 21:07:01 -0300

    chore: update docker configs

--- script.sql
+++ script.sql
@@ -1,13 +1,33 @@
--- Coloque scripts iniciais aqui
-CREATE TABLE clientes;
+SET timezone = 'America/Sao_Paulo';
 
-DO $$
-BEGIN
-INSERT INTO clientes (nome, limite)
-VALUES
-    ('o barato sai caro', 1000 * 100),
-    ('zan corp ltda', 800 * 100),
-    ('les cruders', 10000 * 100),
-    ('padaria joia de cocaia', 100000 * 100),
-    ('kid mais', 5000 * 100);
-END; $$
+CREATE TABLE users
+(
+    id              SERIAL PRIMARY KEY,
+    limit_in_cents  INTEGER NOT NULL,
+    initial_balance INTEGER NOT NULL DEFAULT 0
+);
+
+INSERT INTO users (id, limit_in_cents, initial_balance)
+VALUES (DEFAULT, 1000 * 100, 0),
+       (DEFAULT, 800 * 100, 0),
+       (DEFAULT, 10000 * 100, 0),
+       (DEFAULT, 100000 * 100, 0),
+       (DEFAULT, 5000 * 100, 0);
+
+CREATE UNLOGGED TABLE transactions
+(
+    id          SERIAL PRIMARY KEY,
+    user_id     SMALLINT    NOT NULL,
+    value       INTEGER     NOT NULL,
+    type        CHAR(1)     NOT NULL,
+    description VARCHAR(10) NOT NULL,
+    do_at       TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+ALTER TABLE
+    transactions
+    SET
+    (autovacuum_enabled = false);
+
+CREATE INDEX user_id_transactions ON transactions (user_id);
+
